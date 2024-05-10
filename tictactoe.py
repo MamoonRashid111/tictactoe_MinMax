fd60793502f82419cb10cb7a7fed7cf66522e242
malakafaqahmad <124126284+malakafaqahmad@users.noreply.github.com>
2024-05-10 12:43:12 +0500

    winningPlayer function was updated

--- tictactoe.py
+++ tictactoe.py
@@ -23,3 +23,85 @@ def clearBoard(brd):
     for x, row in enumerate(brd):
         for y, col in enumerate(row):
             brd[x][y] = EMPTY
+
+
+def winningPlayer(brd, player):
+    winningStates = [[brd[0][0], brd[0][1], brd[0][2]],
+                     [brd[1][0], brd[1][1], brd[1][2]],
+                     [brd[2][0], brd[2][1], brd[2][2]],
+                     [brd[0][0], brd[1][0], brd[2][0]],
+                     [brd[0][1], brd[1][1], brd[2][1]],
+                     [brd[0][2], brd[1][2], brd[2][2]],
+                     [brd[0][0], brd[1][1], brd[2][2]],
+                     [brd[0][2], brd[1][1], brd[2][0]]]
+
+    if [player, player, player] in winningStates:
+        return True
+
+    return False
+
+
+def gameWon(brd):
+    return winningPlayer(brd, XPLAYER) or winningPlayer(brd, OPLAYER)
+
+
+def printResult(brd):
+    if winningPlayer(brd, XPLAYER):
+        print('X has won! ' + '\n')
+
+    elif winningPlayer(brd, OPLAYER):
+        print('O\'s have won! ' + '\n')
+
+    else:
+        print('Draw' + '\n')
+
+
+def emptyCells(brd):
+    emptyC = []
+    for x, row in enumerate(brd):
+        for y, col in enumerate(row):
+            if brd[x][y] == EMPTY:
+                emptyC.append([x, y])
+
+    return emptyC
+
+
+def boardFull(brd):
+    if len(emptyCells(brd)) == 0:
+        return True
+    return False
+
+
+def setMove(brd, x, y, player):
+    brd[x][y] = player
+
+
+def playerMove(brd):
+    e = True
+    moves = {1: [0, 0], 2: [0, 1], 3: [0, 2],
+             4: [1, 0], 5: [1, 1], 6: [1, 2],
+             7: [2, 0], 8: [2, 1], 9: [2, 2]}
+    while e:
+        try:
+            move = int(input('Pick a position(1-9)'))
+            if move < 1 or move > 9:
+                print('Invalid location! ')
+            elif not (moves[move] in emptyCells(brd)):
+                print('Location filled')
+            else:
+                setMove(brd, moves[move][0], moves[move][1], XPLAYER)
+                printBoard(brd)
+                e = False
+        except(KeyError, ValueError):
+            print('Please pick a number!')
+
+
+def getScore(brd):
+    if winningPlayer(brd, XPLAYER):
+        return 10
+
+    elif winningPlayer(brd, OPLAYER):
+        return -10
+
+    else:
+        return 0
