38b817729a90e361c76ec1c37f7c02b7c3cedf45
malakafaqahmad <124126284+malakafaqahmad@users.noreply.github.com>
2024-05-10 12:42:01 +0500

    added 2 functions

--- tictactoe.py
+++ tictactoe.py
@@ -9,3 +9,17 @@ board = [[EMPTY, EMPTY, EMPTY],
          [EMPTY, EMPTY, EMPTY],
          [EMPTY, EMPTY, EMPTY]]
 
+def printBoard(brd):
+    chars = {XPLAYER: 'X', OPLAYER: 'O', EMPTY: ' '}
+    for x in brd:
+        for y in x:
+            ch = chars[y]
+            print(f'| {ch} |', end='')
+        print('\n' + '---------------')
+    print('===============')
+
+
+def clearBoard(brd):
+    for x, row in enumerate(brd):
+        for y, col in enumerate(row):
+            brd[x][y] = EMPTY
