23db34aa83dc8b52caa7e9fc634e213d711a6af9
malakafaqahmad <124126284+malakafaqahmad@users.noreply.github.com>
2024-05-10 12:41:15 +0500

    sample

---
+++ tictactoe.py
@@ -0,0 +1,11 @@
+from random import choice
+from math import inf
+
+XPLAYER = +1
+OPLAYER = -1
+EMPTY = 0
+
+board = [[EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY]]
+
